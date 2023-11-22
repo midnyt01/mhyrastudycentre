6952cc46e97889b8634fdffba609e01824b65807
Zishan Sheikh <47268153+zishansheikhh@users.noreply.github.com>
2023-11-22 17:28:46 +0530

    meta implemented

--- client/src/components/contact-me.jsx
+++ client/src/components/contact-me.jsx
@@ -1,33 +1,33 @@
-import {
-  faFacebook,
-  faInstagram,
-  faLinkedin,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext, useState } from "react";
+// import {
+//   faFacebook,
+//   faInstagram,
+//   faLinkedin,
+//   faYoutube,
+// } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import styled from "styled-components";
-import { GlobalContext } from "../context/global.context";
-import { makeid } from "../context/helper-functions";
+// import { GlobalContext } from "../context/global.context";
+// import { makeid } from "../context/helper-functions";
 
 import {
   Button,
   TextField,
   Box,
-  Grid,
+  // Grid,
   Typography,
-  Avatar,
+  // Avatar,
   Container,
   CssBaseline,
-  Checkbox,
-  FormControlLabel,
+  // Checkbox,
+  // FormControlLabel,
   Select,
-  MenuItem,
-  InputLabel,
+  // MenuItem,
+  // InputLabel,
 } from "@mui/material";
 import { useFormik } from "formik";
 import { ContactSchema } from "../schema/schema.index";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { httpPostEnquiry } from "../utils/api";
 
 const MainContainer = styled.div`
@@ -86,98 +86,98 @@ const Title = styled.h3`
   }
 `;
 
-const ContactForm = styled.form`
-  width: 90%;
-  margin: auto;
-  margin-top: 70px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  row-gap: 30px;
-`;
+// const ContactForm = styled.form`
+//   width: 90%;
+//   margin: auto;
+//   margin-top: 70px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-between;
+//   align-items: center;
+//   row-gap: 30px;
+// `;
 
-const FormInput = styled.input`
-  width: 110%;
-  box-sizing: border-box;
-  padding: 15px;
-  font-size: 17px;
-  letter-spacing: 0.7px;
-  font-weight: 400;
-  color: black;
-  border-radius: 8px;
-  background: rgb(254, 254, 254);
-  outline: none;
-  border: none;
-  @media (min-width: 800px) {
-    width: 50%;
-  }
-`;
+// const FormInput = styled.input`
+//   width: 110%;
+//   box-sizing: border-box;
+//   padding: 15px;
+//   font-size: 17px;
+//   letter-spacing: 0.7px;
+//   font-weight: 400;
+//   color: black;
+//   border-radius: 8px;
+//   background: rgb(254, 254, 254);
+//   outline: none;
+//   border: none;
+//   @media (min-width: 800px) {
+//     width: 50%;
+//   }
+// `;
 
-const FromSelect = styled.select`
-  width: 110%;
-  box-sizing: border-box;
-  padding: 15px;
-  font-size: 17px;
-  letter-spacing: 0.7px;
-  font-weight: 400;
-  color: black;
-  border-radius: 8px;
-  background: rgb(254, 254, 254);
-  outline: none;
-  border: none;
-  @media (min-width: 800px) {
-    width: 50%;
-  }
-`;
+// const FromSelect = styled.select`
+//   width: 110%;
+//   box-sizing: border-box;
+//   padding: 15px;
+//   font-size: 17px;
+//   letter-spacing: 0.7px;
+//   font-weight: 400;
+//   color: black;
+//   border-radius: 8px;
+//   background: rgb(254, 254, 254);
+//   outline: none;
+//   border: none;
+//   @media (min-width: 800px) {
+//     width: 50%;
+//   }
+// `;
 
-const FormOption = styled.option`
-  font-size: 17px;
-  font-weight: 400;
-  outline: none;
-  background: rgba(225, 226, 216, 0.5);
-`;
+// const FormOption = styled.option`
+//   font-size: 17px;
+//   font-weight: 400;
+//   outline: none;
+//   background: rgba(225, 226, 216, 0.5);
+// `;
 
-const MessageBox = styled.textarea`
-  width: 110%;
-  box-sizing: border-box;
-  padding: 15px;
-  font-size: 17px;
-  letter-spacing: 0.7px;
-  font-weight: 400;
-  color: black;
-  border-radius: 8px;
-  height: 150px;
-  background: rgb(254, 254, 254);
-  font-family: inherit;
-  outline: none;
-  border: none;
-  @media (min-width: 800px) {
-    width: 50%;
-  }
-`;
+// const MessageBox = styled.textarea`
+//   width: 110%;
+//   box-sizing: border-box;
+//   padding: 15px;
+//   font-size: 17px;
+//   letter-spacing: 0.7px;
+//   font-weight: 400;
+//   color: black;
+//   border-radius: 8px;
+//   height: 150px;
+//   background: rgb(254, 254, 254);
+//   font-family: inherit;
+//   outline: none;
+//   border: none;
+//   @media (min-width: 800px) {
+//     width: 50%;
+//   }
+// `;
 
-const SubmitButton = styled.button`
-  border: 2px solid black;
-  margin-top: 30px;
-  box-sizing: border-box;
-  padding: 12px 40px;
-  letter-spacing: 0.5px;
-  background: white;
-  color: black;
-  font-size: 16px;
-  font-weight: 400;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background: rgb(67, 97, 255);
-  }
-  @media (min-width: 800px) {
-    margin-top: 50px;
-    padding: 15px 50px;
-    font-size: 18px;
-  }
-`;
+// const SubmitButton = styled.button`
+//   border: 2px solid black;
+//   margin-top: 30px;
+//   box-sizing: border-box;
+//   padding: 12px 40px;
+//   letter-spacing: 0.5px;
+//   background: white;
+//   color: black;
+//   font-size: 16px;
+//   font-weight: 400;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   &:hover {
+//     background: rgb(67, 97, 255);
+//   }
+//   @media (min-width: 800px) {
+//     margin-top: 50px;
+//     padding: 15px 50px;
+//     font-size: 18px;
+//   }
+// `;
 
 const HR = styled.hr`
   width: 50%;
@@ -186,32 +186,32 @@ const HR = styled.hr`
   margin-top: 70px;
 `;
 
-const SocialsContainer = styled.div`
-  width: 100%;
-`;
+// const SocialsContainer = styled.div`
+//   width: 100%;
+// `;
 
-const SocialTitle = styled.div`
-  text-align: center;
-  margin-top: 70px;
-  font-size: 20px;
-  color: #000;
-  font-weight: 600;
-  letter-spacing: 0.5px;
-  text-transform: uppercase;
-`;
+// const SocialTitle = styled.div`
+//   text-align: center;
+//   margin-top: 70px;
+//   font-size: 20px;
+//   color: #000;
+//   font-weight: 600;
+//   letter-spacing: 0.5px;
+//   text-transform: uppercase;
+// `;
 
-const SocialWrapper = styled.div`
-  width: 300px;
-  margin: auto;
-  margin-top: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+// const SocialWrapper = styled.div`
+//   width: 300px;
+//   margin: auto;
+//   margin-top: 50px;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
 
-  svg {
-    cursor: pointer;
-  }
-`;
+//   svg {
+//     cursor: pointer;
+//   }
+// `;
 
 const StyledBox = styled(Box)`
   width: 100%;
@@ -239,9 +239,9 @@ const StyledBox = styled(Box)`
     width: 100%;
   }
 `;
-const StyledTypography = styled(Typography)`
-  margin-top: 20px;
-`;
+// const StyledTypography = styled(Typography)`
+//   margin-top: 20px;
+// `;
 
 const DEFAULT_FORM_FIELD = {
   FullName: "",
@@ -299,6 +299,7 @@ const ContactMeForm = () => {
 
   return (
     <MainContainer>
+      <meta name="description" content="Admissions are open to enquire visit our website or contact us." />
       <Wrapper>
         <Title>Contact us</Title>
         <FlexContainer>
